eb15800155e3852f6e24050cd3dc11190c5ef311
sufulu <1760473022@qq.com>
2017-04-17 13:10:20 +0800

    nt

--- app/src/main/java/moive/sus/com/moivexun/api/Api.java
+++ app/src/main/java/moive/sus/com/moivexun/api/Api.java
@@ -12,6 +12,26 @@ import moive.sus.com.moivexun.BuildConfig;
 public class Api {
     private static final String TMDB_API_KEY = BuildConfig.TMDB_API_KEY;
     public static final String BASE_URL = BuildConfig.BASE_URL;
+    //https://api.themoviedb.org/3/movie/{movie_id}/lists?api_key=<<api_key>>&language=en-US&page=1 获取电影列表
+    //https://api.themoviedb.org/3/movie/latest?api_key=<<api_key>>&language=en-US
+    //https://api.themoviedb.org/3/movie/now_playing?api_key=<<api_key>>&language=en-US&page=1
+    //https://api.themoviedb.org/3/movie/popular?api_key=<<api_key>>&language=en-US&page=1
+    //https://api.themoviedb.org/3/movie/top_rated?api_key=<<api_key>>&language=en-US&page=1
+    //https://api.themoviedb.org/3/movie/upcoming?api_key=<<api_key>>&language=en-US&page=1
+    //https://api.themoviedb.org/3/movie/{movie_id}/reviews?api_key=<<api_key>>&language=en-US&page=1
+    //https://api.themoviedb.org/3/movie/{movie_id}/similar?api_key=<<api_key>>&language=en-US&page=1
+    //https://api.themoviedb.org/3/movie/{movie_id}/recommendations?api_key=<<api_key>>&language=en-US&page=1
+    //https://api.themoviedb.org/3/movie/{movie_id}/translations?api_key=<<api_key>>
+    //https://api.themoviedb.org/3/movie/{movie_id}/videos?api_key=<<api_key>>&language=en-US
+    //https://api.themoviedb.org/3/movie/{movie_id}/release_dates?api_key=<<api_key>>
+    //https://api.themoviedb.org/3/movie/{movie_id}?api_key=<<api_key>>&language=en-US
+    //https://api.themoviedb.org/3/movie/{movie_id}/images?api_key=<<api_key>>&language=en-US
+    //https://api.themoviedb.org/3/movie/{movie_id}/keywords?api_key=<<api_key>>
+    //https://api.themoviedb.org/3/movie/{movie_id}/credits?api_key=<<api_key>>
+    //https://api.themoviedb.org/3/movie/{movie_id}/changes?api_key=<<api_key>>&page=1
+    //https://api.themoviedb.org/3/keyword/{keyword_id}?api_key=<<api_key>>
+    //https://api.themoviedb.org/3/keyword/{keyword_id}/movies?api_key=<<api_key>>&language=en-US&include_adult=false
+
     private Api()
     {
         // hide implicit public constructor
